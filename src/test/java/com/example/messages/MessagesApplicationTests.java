4d796cea8f95f3abf173331aeb1ee2c840020011
Atulan Zaman <atulan.zaman@gmail.com>
2021-12-06 13:49:48 -0500

    Add unittest and Postman API test

--- src/test/java/com/example/messages/MessagesApplicationTests.java
+++ src/test/java/com/example/messages/MessagesApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.messages;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class MessagesApplicationTests {
 
+	@MockBean
+	private MessageRepository repository;
+
 	@Test
-	void contextLoads() {
+	void testMessageIsPalindrome() {
+		assertTrue(new Message("abcba").getIsPalindrome());
+		assertTrue(new Message("abba").getIsPalindrome());
+		assertTrue(new Message("c").getIsPalindrome());
+		assertFalse(new Message("abc").getIsPalindrome());
 	}
+
 
 }
